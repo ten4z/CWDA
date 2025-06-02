8c6e3f5cd0d172de056e5676c8138f797beddbcd
Ten4z <zigainteractive@gmail.com>
2025-06-02 09:03:07 -0300

    Adicionado suporte à SQLite

--- app/main.py
+++ app/main.py
@@ -2,12 +2,17 @@ from kivy.app import App
 from kivy.lang.builder import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 import scripts.capitulos as capitulos
+import sqlite3
 
 Builder.load_file("gui/principal.kv")
 
 
 class Sc_Login(Screen):
-	pass
+	def __init__(self, **kwargs):
+		super().__init__(**kwargs)
+		self.current_app = App.get_running_app()
+		self.conexao = self.current_app.conexao
+		self.cursor = self.current_app.cursor
 
 class Sc_Menu(Screen):
 	pass
@@ -19,6 +24,9 @@ class Sc_Manager(ScreenManager):
 	pass
 
 class CWDA(App):
+	conexao = sqlite3.connect("media_data.db")
+	cursor = conexao.cursor()
+
 	def build(self):
 		self.scm = Sc_Manager()
 		capitulos.leitura_cap01()
