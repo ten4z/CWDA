05acc60782815a10f0e13634bd5b2d1e9712a2b4
Ten4z <zigainteractive@gmail.com>
2025-06-02 07:33:28 -0300

    Adicionada a classe do App

---
+++ app/main.py
@@ -0,0 +1,21 @@
+from kivy.app import App 
+from kivy.lang.builder import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+Builder.load_file("gui/principal.kv")
+
+class Sc_Menu(Screen):
+	pass
+
+class Sc_Manager(ScreenManager):
+	pass
+
+class CWDA(App):
+	def build(self):
+		self.scm = Sc_Manager()
+		return self.scm
+
+
+
+if __name__ == "__main__":
+	CWDA().run()
