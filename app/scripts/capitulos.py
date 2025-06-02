6ff25d46af227aca3f48d752726763f05502de63
Ten4z <zigainteractive@gmail.com>
2025-06-02 08:10:07 -0300

    Método de exercícios do livro

--- app/scripts/capitulos.py
+++ app/scripts/capitulos.py
@@ -8,7 +8,23 @@ Builder.load_file("gui/principal.kv")
 class Sc_Capitulos(Screen):
 	def leitura_c1(self):
 		print("capitulo 1 página 1")
+	def exercicios_c1(self):
+		print("exercício página 20")
+	def solucao_exercicio(self, ex, md):		
+		if md == "m1":
+			print("solucionar exercício " +ex + " com papel e caneta")
+		elif md == "m2":
+			print("solucionar exercício " +ex + " com calculadora")
+		elif md == "m3":
+			print("solucionar exercício " +ex + " com algoritmo")
+		elif md == "m4":
+			print("solucionar exercício " +ex + " com software")
+		elif md == "m5":
+			print("solucionar exercício " +ex + " modo completo")
+
 
 def leitura_cap01():
 	Sc_Capitulos().leitura_c1()
+	Sc_Capitulos().exercicios_c1()
+	Sc_Capitulos().solucao_exercicio("ex1", "m5")
 
