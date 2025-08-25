7cde4d2055b9c83e3e864b76ecab9e60bcc5a536
Ten4z <zigainteractive@gmail.com>
2025-08-25 05:11:13 -0300

    modelagem código ExDez

--- lab/ExDez.py
+++ lab/ExDez.py
@@ -4,11 +4,16 @@ class ExDez():
    num = 10
    den = 0
    cont = 0
-   expr = "x**2 + 13*x + 40"
-   
+   expr = "x**2 + 13*x + 40" 
    def posNums(self, n, d):
+      '''
+      Este método é para encontrar números racionais\n
+      do tipo n/d, no intervalo de -10 a 10, \n
+      para compararmos na fórmula se este\n
+      número é raiz da quadrática.
+      '''
       if n >= -10 and n <= 10:
-         if self.den >= -10 and self.den <= 10:
+         if self.den >= (-10) and self.den < 10:
             self.den += 1
          else:
             self.num -= 1
@@ -18,21 +23,25 @@ class ExDez():
          self.num += 1
          self.den += 1
          n = self.num
-         
-      print("num = ", self.num, "den = ", self.den)
-      
-      x = self.num / self.den
-      print(str(self.num) + "/" + str(self.den) + " = " + str(x))
-      print("resultado:")
-      print(eval(self.expr))
       self.cont += 1
-
-      if self.num >= (-10) and self.num <= 10:
-         try:
-            self.posNums(n, d)
-         except:
-            print("Impossível dividir por zero.")
-            
-      
+      #print("num = ", self.num, "den = ", self.den)    
+      if  self.den > 0 or  self.den < 0:
+          x = self.num/self.den
+          #print(eval(self.expr))
+          if eval(self.expr) == 0:
+             print(x)
+             '''
+             Que na linha acima resultou em três números com os\n
+             números -5, -8 -5, o menos cinco se repetiu, e vamos\n
+             utilizar os outros dois números, o -5 e -8 \n
+             dado o enunciado, vamos subtrair a menor raiz\n
+             da maior raiz assim: -5 -(-8) =  3,\n
+             resposta letra "a"
+             '''
+      else:
+          pass
+      if self.num >= (-10) and self.num <= 10:         
+         self.posNums(n, d)
+         
 if __name__ == "__main__":
    ExDez().posNums(1, -10)
